7439a6fe3fa7a9ae5aaa8d4fccf240cbc5d0e7ec
Repositorio Desarrollo y Diseño Web DuocUC Viña del Mar <109529102+ddwduocvina@users.noreply.github.com>
2024-06-27 14:53:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-$.ajax({
-    url: 'ingles.html',
-    type: 'get',
-    success: function(respuesta){
-        let contenidoTraducido = document.getElementById('contenidoTraduccion')
-        contenidoTraducido.innerHTML = respuesta
-    },
-    error: function(){
-        console.log('No carga el archivo');
-    }
-})
+$('document').ready(function(){
+    $.ajax({
+        url: 'ingles.html',
+        type: 'get',
+        success: function(respuesta){
+            let contenidoTraducido = document.getElementById('contenidoTraduccion')
+            contenidoTraducido.innerHTML = respuesta
+        },
+        error: function(){
+            console.log('No carga el archivo');
+        }
+    })
+
+    $('#traductor').click(function(){
+        $(this).toggleClass('idioma');
+    });
+});
